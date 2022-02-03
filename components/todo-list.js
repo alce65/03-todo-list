4042fd2da4be717235fef9f9770868ef6a931673
Alejandro Cerezo <alce65@hotmail.es>
2022-02-03 12:32:51 +0100

    Update TodoList

--- components/todo-list.js
+++ components/todo-list.js
@@ -1,3 +1,4 @@
+import { STORE_NAME } from '../config.js';
 import { Component } from './component.js';
 
 export class TodoList extends Component {
@@ -8,7 +9,7 @@ export class TodoList extends Component {
         this.tasks = tasks;
         this.template = this.generateTemplate();
         this.render('#tasks');
-        // this.manageComponent();
+        this.manageComponent();
     }
     generateTemplate() {
         let template = `
@@ -18,13 +19,32 @@ export class TodoList extends Component {
                     Lista de tareas
                 </h3>
                 <ul>`;
-        this.tasks.forEach((item) => {
-            template += `<li>${item}</li>`;
+        this.tasks.forEach((item, i) => {
+            template += `
+                <li>${item}
+                    <span role="button" style="cursor:pointer" data-id="${i}">
+                    🗑️
+                    </span>
+                </li>`;
         });
         template += `
                 </ul>
             </div>
         `;
         return template;
+    }
+    manageComponent() {
+        const component = document.querySelector('#tasks');
+        [...component.querySelectorAll('[role]')].forEach((item) =>
+            item.addEventListener('click', (ev) => {
+                this.deleteItems(+ev.target.dataset.id);
+            })
+        );
+    }
+    deleteItems(id) {
+        this.tasks.splice(id, 1);
+        // this.tasks.filter(item => item.id !== id)
+        localStorage.setItem(STORE_NAME, JSON.stringify(this.tasks));
+        location.reload();
     }
 }
